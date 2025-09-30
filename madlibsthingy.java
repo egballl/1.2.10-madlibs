ef59f41572c36189ea85f2b11c290aaef895a0dc
egballl <148280195+egballl@users.noreply.github.com>
2025-09-30 14:11:00 -0700

    Create madlibsthingy.java

---
+++ madlibsthingy.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class madlibsthingy {
+    public static void main(String[] args) {
+        Scanner word = new Scanner(System.in);
+        String word1 = word.nextLine();
+
+        System.out.println(word1);
+    }
+}
