64d948c87bc9cf9da99f6b33e26bef1fa82ed6a4
egballl <148280195+egballl@users.noreply.github.com>
2025-10-10 14:20:42 -0700

    Update Hung_MadLibs.java

--- Hung_MadLibs.java
+++ Hung_MadLibs.java
@@ -8,8 +8,8 @@ public class Hung_MadLibs {
         String defaultStory = "Hello, my name is <noun>. Today, I <verb> to the <adjective> store and bought a <adjective> <noun>. On the way home, I saw a <adjective> <noun> that was <verb> by the <noun>. Later, I <verb> with my <adjective> <noun> until the <noun> started to <verb>. It was the most <adjective> day ever!";
         System.out.println("Mad Libs 9,000,000!");
         System.out.println("Choose an option:");
-        System.out.println("1. Use premade adventure story (15 words)");
-        System.out.println("2. Enter your own custom story");
+        System.out.println("1. Use premade story (15 words)");
+        System.out.println("2. Enter your story");
         System.out.print("Enter your choice (1-2): ");
         int choice = input.nextInt();
         input.nextLine();
@@ -31,7 +31,7 @@ public class Hung_MadLibs {
                 selectedStory += " The <adjective> end!";
             }
         } else {
-            System.out.println("Invalid. Using premade adventure story.");
+            System.out.println("Invalid. Using premade story.");
             selectedStory = defaultStory;
         }
         
@@ -58,11 +58,13 @@ public class Hung_MadLibs {
                 System.out.print("Enter a " + wordType + " (#" + wordCount + "): ");
                 String userWord = input.nextLine();
                 // Replace the first occurrence of the placeholder
-                String placeholder = "<" + wordType + ">";
-                //result = result.replaceFirst(placeholder, userWord);   - change this later
+                //next two lines aren't needed
+                //String placeholder = "<" + wordType + ">";
+                //result = result.replaceFirst(placeholder, userWord);
+                result =  result.substring(0,startIndex) + userWord + result.substring(endIndex + 1); // - change this
                 wordCount++;
             } else {
-                break; // No more placeholders
+                break;
             }
         }
         
