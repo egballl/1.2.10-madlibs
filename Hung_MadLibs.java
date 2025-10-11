c8290ebb84cf6683884e13be61f38a9cc10677c8
egballl <148280195+egballl@users.noreply.github.com>
2025-10-10 23:47:15 -0700

    added multi line comment at start for info about program

--- Hung_MadLibs.java
+++ Hung_MadLibs.java
@@ -1,12 +1,22 @@
+/*
+ * Hung_MadLibs2.java
+ * Mad Libs program that accepts user input for nouns, verbs, and adjectives
+ * and inserts them into a story template.
+ * (provided program reader with information about program)
+ */
+
 import java.util.Scanner;
 
-public class Hung_MadLibs2{
+public class Hung_MadLibs{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        // Premade story (15 inputs)
+        // Premade story
         String defaultS = "Hello, my name is <noun>. Today, I <verb> to the <adjective> store and bought a <adjective> <noun>. On the way home, I saw a <adjective> <noun> that was <verb> by the <noun>. Later, I <verb> with my <adjective> <noun> until the <noun> started to <verb>. It was the most <adjective> day ever!";
-        
+        /* Test Cases
+        1. The <noun> <verb> across the <adjective> pond
+        2. I went to the zoo to see a <noun>. There were people <verb> around the enclosure which made the <adjective> apes go bananas. */
+
         System.out.println("Choose an option:");
         System.out.println("1. Use premade story (15 words)");
         System.out.println("2. Enter your story");
@@ -21,7 +31,7 @@ public class Hung_MadLibs2{
             System.out.println("Enter your test case below.");
             System.out.println("Use <noun>, <verb>, and <adjective> as placeholders.");
             System.out.println("Ex: The <adjective> <noun> <verb> quickly.\n");
-            System.out.print("Your Story: ");
+            System.out.print("Enter Your Story: ");
             story = input.nextLine();
             if (story.indexOf("<") == -1) {
                 System.out.println("Your story has no placeholders!");
